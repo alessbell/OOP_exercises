077d1925a160dc80667e09af42107bf0cd18967a
Alessia Bellisario <bellisario.alessia@gmail.com>
2014-06-05 13:57:51 -0400

    Added mars.rb

---
+++ mars.rb
@@ -0,0 +1,19 @@
+class Plateau
+	attr_accessor :width, :height
+
+	def initialize(width, height)
+		@width = width
+		@height = height
+	end
+end
+
+class Rover
+	attr_accessor :x, :y, :orientation, :plateau
+		@x = y
+		@y = y
+		@orientation = orientation
+		@plateau = plateau
+	end
+	
+	def instruction(str)
+
--- people.rb
+++ people.rb
@@ -1,10 +1,8 @@
 class Person
-	attr_reader :name 
-
+	attr_reader :name
 	def initialize(name)
 		@name = name
 	end
-
 	def to_s
 		"Hi, my name is #{@name}."
 	end
@@ -25,11 +23,14 @@ end
 Instructor1 = Instructor.new("Chris")
 Student1 = Student.new("Cristina")
 
+puts Instructor1
+puts Student1
+
 puts Instructor1.teach
 puts Student1.learn
 
-puts Instructor1.learn
-puts Student1.teach
+# puts Instructor1.learn
+# puts Student1.teach
 
 # calling learn on Instructor1 and teach on Student1 doesn't work because
 # an undefined method error is returned due to methods being in different
