6f7e7037f4987dbbc36d400aa68a4e3efba44cd9
Alessia Bellisario <bellisario.alessia@gmail.com>
2014-06-05 16:38:14 -0400

    Added mars.rb

--- mars.rb
+++ mars.rb
@@ -1,19 +1,69 @@
-class Plateau
-	attr_accessor :width, :height
-
-	def initialize(width, height)
-		@width = width
-		@height = height
-	end
+class Plateau 
+  def initialize(max_x, max_y)
+    @max_x = max_x
+    @max_y = max_y
+  end
 end
 
 class Rover
-	attr_accessor :x, :y, :orientation, :plateau
-		@x = y
-		@y = y
-		@orientation = orientation
-		@plateau = plateau
-	end
-	
-	def instruction(str)
+  def initialize(x, y, direction, orders)
+    @x = x
+    @y = y
+    @direction = direction
+    @orders = orders
+  end
 
+  def move
+
+    case @direction
+      when "N"
+        @y += 1
+      when "E"
+        @x += 1
+      when "S"
+        @y -= 1
+      when "W"
+        @x -= 1
+    end
+
+  end
+
+  def rotate(instruction)
+    cardinals = ["N", "E", "S", "W"]
+
+    current_position = cardinals.index(@direction)
+
+    if instruction == "L"
+
+      current_position -= 1
+
+    else
+      current_position += 1
+      current_position = 0 if current_position > 3
+    end
+    
+    @direction = cardinals[current_position]
+  
+  end
+
+  def get_new_position()
+    
+    @orders.each_char do |x|
+      if x == "L" || x == "R"
+        rotate(x)
+      else
+        move
+      end
+    end
+  
+  puts "#{@x} #{@y} #{@direction}"
+
+  end
+
+end
+
+rectangle_1 = Plateau.new(5, 5)
+rover_1 = Rover.new(1, 2, "N", "LMLMLMLMM")
+rover_1.get_new_position
+rover_2 = Rover.new(3, 3, "E", "MMRMMRMRRM")
+rover_2.get_new_position
